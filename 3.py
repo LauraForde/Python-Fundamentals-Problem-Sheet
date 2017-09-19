31fcf12dd7e531dca528fe3767eb488f05732214
LauraForde <G00315765@gmit.ie>
2017-09-19 22:27:39 +0100

    Printing 1 - 100 to ensure is working

--- 3.py
+++ 3.py
@@ -1,3 +1,9 @@
 #Exercise 3, FizzBuzz
 #Print numbers from 1 - 100, multiples of 3 print Fizz
 #multipes of 5 print Buzz, multiples pf 3 & 5 print FizzBuzz
+
+count = 0
+
+for i in range(1,101):
+    count = count = i
+    print(count)
