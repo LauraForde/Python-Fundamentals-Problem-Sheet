9e5469d4629bfc09e7a1be7c6abc977aa2d8b735
LauraForde <G00315765@gmit.ie>
2017-09-19 22:15:16 +0100

    Adding comments

--- 2.py
+++ 2.py
@@ -1,8 +1,12 @@
 # Exercise 2 for the Python Fundamentals work sheet
 # Print the current date and time to the screen
+
 # Adapted from https://www.cyberciti.biz/faq/howto-get-current-date-time-in-python/
 
 from datetime import datetime
 
+#Obtaining the current date and time
 dt = datetime.now()
+
+#Printing the current date and time on seperate lines
 print(dt.strftime("The date is: %d-%m-%Y \nThe time is: %H:%M:%S"))
