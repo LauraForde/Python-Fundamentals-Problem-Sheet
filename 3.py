ed5b914312d8e3e4d74a829c1ea18c262ccd58e3
LauraForde <G00315765@gmit.ie>
2017-09-19 23:01:19 +0100

    Working properly

--- 3.py
+++ 3.py
@@ -2,18 +2,18 @@
 #Print numbers from 1 - 100, multiples of 3 print Fizz
 #multipes of 5 print Buzz, multiples pf 3 & 5 print FizzBuzz
 
-count = 0
+count = 1
 
-#for loop printing 1 - 100, without FizzBuzz
+#for loop with conditions to print Fizz Buzz
 for i in range(1,101):
-    if(count % 5) == 0 and (count % 3) == 0:
-        print("FizzBuzz")
-        count = count + 1
-    if(count % 5) == 0:
-        print("Buzz")
-        count = count + 1
     if(count % 3) == 0:
         print("Fizz")
+        count = count + 1
+    elif(count % 5) == 0:
+        print("Buzz")
+        count = count + 1
+    elif(count % 5) == 0 and (count % 3) == 0:
+        print("FizzBuzz")
         count = count + 1
     else:
         print(count)
