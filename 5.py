1443f7641924c3c0dff8e7aa33ba2939a31c19df
LauraForde <G00315765@gmit.ie>
2017-09-20 11:49:46 +0100

    Basic output and input, not looping or calculating

--- 5.py
+++ 5.py
@@ -1,1 +1,15 @@
 #Exercise 5, Guessing Game
+
+number = 5
+
+guess = int(input("Guess the number? "))
+
+if guess == number:
+    print("Correct!!")
+elif guess < number:
+    print("Your guess is too low.")
+elif guess > number:
+    print("Your guess is too high")
+else:
+    print("Bad guess")
+
