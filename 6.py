a8477f76d272750b14b4d96bb111723203941fc4
LauraForde <G00315765@gmit.ie>
2017-09-20 13:09:04 +0100

    Showing min and max numbers

--- 6.py
+++ 6.py
@@ -1,6 +1,12 @@
 #Exercise 6, Print smallest and largest element in list
+#Reading into list adapted from https://stackoverflow.com/questions/4663306/get-a-list-of-numbers-as-input-from-the-user
+#Adapted from https://medium.com/programminginpython-com/python-program-to-find-the-largest-and-smallest-number-in-a-list-fd8fac8aba08
 
-getList = input("Enter 3 Numbers: ")
-numbers = list(map(int, getList.split()))
+getList = [] 
+manyNum = int(input("How many numbers will you enter? "))
 
-print(numbers)
+for i in range(manyNum):
+    numEntered = int(input("Enter Number: "))
+    getList.append(numEntered)
+
+print("The max number is:", max(getList), "\nThe min number is:",min(getList))
