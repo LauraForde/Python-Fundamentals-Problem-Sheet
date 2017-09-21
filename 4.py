42964e44a4b308cbd5212891c396d7e0cd938676
LauraForde <G00315765@gmit.ie>
2017-09-21 14:19:13 +0100

    Doing the digit sum of 100!, didn't see it earlier

--- 4.py
+++ 4.py
@@ -1,8 +1,11 @@
 #Exercise 4, Factorial Digit Sum
+#Getting the digit sum of 100! adapted from https://discuss.codechef.com/questions/56854/computing-sum-of-digits-of-large-factorial
 
 #Getting input from console
+from math import factorial
 number = int(input("What number do you want the factorial of? "))
-factorial = 1
+fact = 1
+digitSum = 0
 
 #Saying factorial does not exist for negative numbers
 if number < 0:
@@ -13,5 +16,13 @@ elif number == 0:
 # Else loop through and multiply
 else:
     for i in range(1,number + 1):
-        factorial = factorial*i
-    print("Factorial is: ", factorial)     
+        fact = fact*i
+    print("Factorial is: ", fact)  
+
+#Getting the sum of all the digits in 100!
+fac = factorial(100)
+fac =str(fac)
+
+for i in fac:
+    digitSum+=int(i)
+print("The sum of the digits in 100! is:",digitSum)
---
+++ 8.py
@@ -0,0 +1,5 @@
+#Exercise 8, merging and sorting two lists
+
+list1 = [] 
+list2 = [] 
+
