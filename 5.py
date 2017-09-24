7b85921b84e3973d02c601fd975c1d38d7514e6d
LauraForde <G00315765@gmit.ie>
2017-09-24 14:40:54 +0100

    Editing exercise 5 to error handle when user enters number out of the range specified

--- 5.py
+++ 5.py
@@ -2,24 +2,35 @@
 #Adapted from https://www.tutorialspoint.com/python/python_while_loop.htm
 #and https://wiki.python.org/moin/WhileLoop
 #random number adapted from https://pythonspot.com/en/random-numbers/
+#number not in range adapted from https://stackoverflow.com/questions/11594605/python-excepting-input-only-if-in-range
 
 from random import *
-number = randint(1,10)
+number = randint(1,11)
 count = 1
 
-guess = int(input("Guess the number? "))
+#guess = int(input("Guess a number between 1 & 10: "))
 
-while(guess != number):
-    
-    if guess < number:
-        print("Your guess is too low.")
-        count += 1
-    elif guess > number:
-        print("Your guess is too high.")
-        count += 1
-    guess = int(input("Guess the number? "))
-
-print("Correct! It took ", count, " guesses." )
+while True:
+    try:
+        guess = int(input("Guess a number between 1 & 10: "))
+    except ValueError:
+        print("Invalid input.")
+    else:
+        if 1<= guess < 11:
+            while(guess != number):
+                
+                if guess < number:
+                    print("Your guess is too low.")
+                    count += 1
+                elif guess > number:
+                    print("Your guess is too high.")
+                    count += 1
+                guess = int(input("Guess the number? "))
+            
+            print("Correct! It took ", count, " guesses." )
+            break
+        else:
+            print("Number is out of range!\nTake another guess:")
 
 #Rough work
 '''if guess == number:
